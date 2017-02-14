e3dfe816963c7c378eeca33989ed12eac1e76760
Jennifer Minnich <jminniecc@gmail.com>
2017-02-13 20:28:53 -0700

    commit sticky footer

--- index.php
+++ index.php
@@ -2,23 +2,28 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
 
 		<!-- jQuery -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
@@ -32,7 +37,8 @@
 					<div class="container">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#main-menu" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
@@ -53,29 +59,70 @@
 
 			<main>
 				<div class="container">
+
 					<!-- row 1-->
 					<div class="welcome">
-						<div class="col-md-6 col-md-offset-5">WELCOME MESSAGE</div>
-					</div>
+						<div class="col-md-6 col-md-offset-5">WELCOME MESSAGE
+						</div>
 					</div>
 
-				<br>
-				<br>
-				<br>
-				<br>
-
-				<div class="image1">
-					<div class="col-md-4">
-						<img src="http://s.hswstatic.com/gif/cassiopeia-constellation-st.jpg" class="img-rounded">
+					<br>
+					<br>
+					<br>
+					<br>
+					<div class="row">
+						<div class="image1">
+							<div class="col-md-4">
+								<img src="http://s.hswstatic.com/gif/cassiopeia-constellation-st.jpg" class="img-rounded">
 							</div>
 						</div>
 
-				<div class="text1">
-					<div class="col-md-5">
-						con·stel·la·tion
-						ˌkänstəˈlāSH(ə)n/
-						noun
-						a group of stars forming a recognizable pattern that is traditionally named after its apparent form or identified with a mythological figure. Modern astronomers divide the sky into eighty-eight constellations with defined boundaries. a group or cluster of related things. "no two patients ever show exactly the same constellation of symptoms"
+						<div class="text1">
+							<div class="col-md-6">
+								con·stel·la·tion
+								ˌkänstəˈlāSH(ə)n/
+								noun
+								a group of stars forming a recognizable pattern that is traditionally named after its apparent
+								form
+								or identified with a mythological figure. Modern astronomers divide the sky into eighty-eight
+								constellations with defined boundaries. a group or cluster of related things. "no two patients
+								ever
+								show exactly the same constellation of symptoms"
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+
+
+					<!-- row 2-->
+					<div class="row">
+						<div class="image2">
+							<div class="col-md-3">
+								<img src="image/img.jpg" class="img-circle">
+								<p>HEADING & TEXT</p>
+							</div>
+						</div>
+
+
+						<div class="image2">
+							<div class="col-md-3">
+								<img src="image/img.jpg" class="img-circle">
+								<p>HEADING & TEXT</p>
+							</div>
+						</div>
+
+
+						<div class="image2">
+							<div class="col-md-3">
+								<img src="image/img.jpg" class="img-circle">
+								<p>HEADING & TEXT</p>
+							</div>
+						</div>
+
+
+						<div class="image2">
+							<div class="col-md-3">
+								<img src="image/img.jpg" class="img-circle">
+								<p>HEADING & TEXT</p>
+							</div>
+						</div>
+					</div>
